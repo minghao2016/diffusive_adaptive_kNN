97c673c620b573d005cbbb586c7b1776409f311d
Manasa <manasa.venkatakrishnan@gmail.com>
2015-11-15 14:57:33 -0700

    Updating DN classification to majority based

--- sortedEuclideanDistance.m
+++ sortedEuclideanDistance.m
@@ -2,9 +2,10 @@ function [ distances,indices ] = sortedEuclideanDistance( X, testPoint)
 %sortedEuclideanDistance Sorted EuclideanDistance from a sample point to 
 %                        all points in data X.
 %   Take distances to all points in X, sort and return them.
-
-t = repmat(testPoint,size(X,1),1);
-euclideanDistances = sqrt(sum((t-X).^2 ,2));
-[distances,indices] = sort(euclideanDistances);
+%   repeats the testpoint vector, vertically as many times as the number of 
+%   points in X and only once horizontally.
+repated_test_point = repmat(testPoint, size(X, 1), 1); 
+euclideanDistances = sqrt(sum((repated_test_point - X).^2, 2));
+[distances, indices] = sort(euclideanDistances);
 end
 
