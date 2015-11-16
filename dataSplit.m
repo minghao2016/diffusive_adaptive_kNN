35fef42157f130f4a3a9b91415d96eea9fa0852d
Purushotham Kaushik Swaminathan <purukaushik@asu.edu>
2015-11-15 17:36:48 -0700

    updated to return corresponding labels.

--- dataSplit.m
+++ dataSplit.m
@@ -1,12 +1,15 @@
-function [X1,X2] = dataSplit( X,y, class1, class2)
+function [X1,X2,Y] = dataSplit( X,y, class1, class2)
 N = size(X,1);
 X1 = [];
 X2 = [];
+Y =[];
 for k = 1:N
     if y(k,1) == class1
         X1 = vertcat(X1,X(k,:));
+        Y = vertcat(Y,1);
     elseif y(k,1) == class2
         X2 = vertcat(X2,X(k,:));
+        Y = vertcat(Y,2);
     end
 end
     
