2df026bf71100ca338e663991ba7cf5ce2faf280
Manasa <manasa.venkatakrishnan@gmail.com>
2015-11-15 18:21:25 -0700

    Label seperation fixed for DataSplit

--- dataSplit.m
+++ dataSplit.m
@@ -1,15 +1,16 @@
-function [X1,X2,Y] = dataSplit( X,y, class1, class2)
+function [X1,X2,Y1, Y2] = dataSplit( X, y, class1, class2)
 N = size(X,1);
 X1 = [];
 X2 = [];
-Y =[];
+Y1 =[];
+Y2 =[];
 for k = 1:N
     if y(k,1) == class1
         X1 = vertcat(X1,X(k,:));
-        Y = vertcat(Y,1);
+        Y1 = vertcat(Y1,1);
     elseif y(k,1) == class2
         X2 = vertcat(X2,X(k,:));
-        Y = vertcat(Y,2);
+        Y2 = vertcat(Y2,2);
     end
 end
     
