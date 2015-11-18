079a20e16fb1f0570a82fa4a80af6f802bef2333
Purushotham Kaushik Swaminathan <purukaushik@asu.edu>
2015-11-18 16:09:47 -0700

    Added comments; return K=N1+N2

--- deltaNRule.m
+++ deltaNRule.m
@@ -1,22 +1,32 @@
-function [ prediction ] = deltaNRule(X1, X2, zN, test)
-%deltaNRule Stops and classifies when DeltaN reaches zN
-%   Diffusion Decision Making for Adaptive k-Nearest Neighbor 
-%   Classification, Yung-Kyun Noh, Frank Chongwoo Park, Daniel D. Lee, 2012
+function [ prediction, k ] = deltaNRule(X1, X2, zN, test)
+%deltaNRule Classify when DeltaN reaches zN
+%   Adaptive rule for nearest neighbour classification.
+%   X1, X2 - training data for class1 and class2 respectively
+%   test - test sample
+%   zN - confidence level
     
+    %compute distance to the two classes
     dist_X1 = sortedEuclideanDistance(X1, test);
     dist_X2 = sortedEuclideanDistance(X2, test);
 
+    %initially take one neighbour to each class
     N1 = 1;
     N2 = 1;
+    
     while true
+        %find the nearer neighbor and include more of nearer class
         if(dist_X1(N1, 1) > dist_X2(N2, 1))
             N2 = N2+1;
         else
             N1 = N1+1;
         end
+        
+        %measuring total no. of neighbors
         k = N1+N2;
+        
         delN = abs(N1-N2);
-                    
+        
+        %check if evidence reached confidence level and classify
         if delN >= zN || N1>size(dist_X1,1) || N2>size(dist_X2,1)            
             if N1 > N2
                 prediction = 1;
