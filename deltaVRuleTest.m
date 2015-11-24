7c4f29d591f8254bf6f160e04d29ffa25da2b451
Manasa <manasa.venkatakrishnan@gmail.com>
2015-11-24 15:23:04 -0700

    Updated DeltaV to test on training data

--- deltaVRuleTest.m
+++ deltaVRuleTest.m
@@ -1,8 +1,9 @@
-function accuracy_array = deltaVRuleTest(training_data, training_label, test_data, test_label, zv_max)
+function accuracy_array = deltaVRuleTest(training_data, training_label, zv_max)
+
 
 accuracy_array = [];
 [filtered_data,~] = compute_mapping(horzcat(training_label,training_data), 'FDA', 9);
-[filtered_test_data,~] = compute_mapping(horzcat(test_label,test_data),'FDA',9);
+%[filtered_test_data,~] = compute_mapping(horzcat(test_label,test_data),'FDA',9);
 for class1 = 0:9
     for class2 = class1+1 : 9
         accuracy = 0;
@@ -11,17 +12,29 @@ for class1 = 0:9
         class1
         class2
         [x1, x2, y1, y2] = dataSplit(filtered_data, training_label, classes(1,1), classes(2,1));
-        [test_x1, test_x2, test_y1, test_y2] = dataSplit(filtered_test_data, test_label, classes(1,1), classes(2,1));
-        test_data = vertcat(test_x1, test_x2);
-        test_label_for_classes = vertcat(test_y1, test_y2);
-        test_size = size(test_label_for_classes, 1);
+        temp_size = size(x1, 1);
+        test_x1 = x1( 4 * temp_size /5 + 1 : temp_size, :); 
+        x1 = x1(1:4 * temp_size /5, :);
+        temp_size = size(x2, 1);
+        test_x2 = x2( 4 * temp_size/5 + 1 : temp_size, :);
+        x2 = x2(1 : 4 * temp_size /5, :);
+        temp_size = size(y1, 1);
+        test_y1 = y1(4 * temp_size/5 + 1 : temp_size, :);
+        y1 = y1(1:4 * temp_size /5, :);
+        temp_size = size(y2, 1);
+        test_y2 = y2( 4 * temp_size/5 + 1 : temp_size, :);
+        y2 = y2(1:4 * temp_size /5, :);
 
-        for z = 1:zv_max
+        test_data = vertcat(test_x1, test_x2);
+        test_label = vertcat(test_y1, test_y2);
+        test_size = size(test_label, 1);
+
+        for z = 0.0001:0.0001:zv_max
             predict = zeros(test_size, 1);
             success = 0;
             for i=1:test_size
                 predict(i) = deltaVRule(x1, x2, z, test_data(i,:), classes);
-                if test_label_for_classes(i, 1) == predict(i, 1)
+                if test_label(i, 1) == predict(i, 1)
                     success = success + 1;
                 end
             end
