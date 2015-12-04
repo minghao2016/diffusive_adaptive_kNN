113da079df10e9aaf2083901c7c5d215bac35c80
Manasa <manasa.venkatakrishnan@gmail.com>
2015-12-03 19:46:11 -0700

    CDV changes

--- conservativeDeltaVRule.m
+++ conservativeDeltaVRule.m
@@ -12,21 +12,19 @@ function [ prediction, N ] = conservativeDeltaVRule(X1, X2, zV, test, classes)
     %initially take one neighbour to each class
     N = 1;
     
-    while true
+    while N < size(dist_X1,1) && N < size(dist_X2,1) 
         %find the difference in distance for the Nth closest point in both classes                
-        d1 =dist_X1(N, 1);
-        d2 =dist_X2(N, 1);
-        if d1<d2 && N < size(dist_X1,1)
-            d1 = dist_X1(N+1,1);
+        d1 = dist_X1(N, 1);
+        d2 = dist_X2(N, 1);
+        if  dist_X1(N+1, 1) < dist_X2(N+1, 1)
+            delV = abs(dist_X1(N+1, 1) - d2);
         else
-            if N < size(dist_X2,1)
-                d2 = dist_X2(N+1,1);
-            end
+            delV = abs(dist_X2(N+1, 1) - d1);
         end
-        delV = abs(d1 - d2);
+        
          
         %check if evidence reached confidence level and classify
-        if delV >= zV || N >= size(dist_X1,1) || N >= size(dist_X2,1)            
+        if delV >= zV || N+1 >= size(dist_X1,1) || N+1 >= size(dist_X2,1)            
             if dist_X1(min([N, size(dist_X1,1)]), 1) < dist_X2(min([N, size(dist_X2,1)]), 1) 
                 prediction = classes(1,1);
             else
